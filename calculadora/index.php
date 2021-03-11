80009de457416d82233b3e5a6e7ee4b27a62d5d7
Emerson916 <emersons.a296@gmail.com>
2021-03-10 22:08:20 -0300

    Atualizações 10/03/21

--- calculadora/index.php
+++ calculadora/index.php
@@ -1,44 +1,81 @@
+<?php
+ 
+    $operacao =  isset($_POST["operacao"]) ? $_POST["operacao"] : null;
+    $numero1 =  isset($_POST["numero1"]) ? $_POST["numero1"] : null;
+    $numero2 = isset($_POST["numero2"]) ? $_POST["numero2"] : null;
+
+
+
+    switch($operacao){
+        case "soma":
+            $resultado = $numero1 + $numero2;
+            break;
+
+        case "subtracao":
+            $resultado = $numero1 - $numero2;
+            break;
+
+        case "multiplicacao":
+            $resultado = $numero1 * $numero2;
+            break;
+    
+        case "divisao":
+            $resultado = $numero1 / $numero2;
+            break;
+
+        default:
+            $resultado = null;
+            break;
+    }
+
+?>
+
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles-global.css" />
     <title>Calculadora</title>
 </head>
 <body>
+    <div class = "input-group">
     <form method="POST" action="index.php">
         <h1>Calculadora</h1>
 
-        <div>
-            <label for="numero1">Número 1</label>
+      
+            <label for="numero1">Número_1</label>
             <input type="number" id="numero1" name="numero1" required/>
-        </div>
-
-        <div>
-            <label for="numero2">Número 2</label>
+       
+       
+            <label for="numero2">Número_2</label>
             <input type="number" id="numero2" name="numero2" required/>
-        </div>
+       
+       
+            <label for="soma">Adição</label>
+            <input type="radio" id="soma" name="operacao" required value="soma"/>
+       
+       
+            <label for="subtracao">Subtração</label>
+            <input type="radio" id="subtracao" name="operacao" required value="subtracao"/>
+       
+        
+            <label for="multiplicacao">Multiplicação</label>
+            <input type="radio" id="multiplicacao" name="operacao" required value="multiplicacao"/>
+       
+       
+            <label for="divisao">Divisão</label>
+            <input type="radio" id="divisao" name="operacao" required value="divisao"/>
 
-        <div>
-        <label for="mais">Adição (+)</label>
-            <input type="radio" id="mais" name="operacao" required/>
-        </div>
-
-        <label for="menos">Subtração (-)</label>
-            <input type="radio" id="menos" name="operacao" required/>
-        </div>
-
-        <label for="vezes">Multiplicação (x)</label>
-            <input type="radio" id="vezes" name="operacao" required/>
-        </div>
-
-        <label for="dividir">Divisão (/)</label>
-            <input type="radio" id="dividir" name="operacao" required/>
-        </div>
+            <label for="resultado">Resultado </label>
+            <input type="text" id="resultado" readonly value="<?=$resultado?>"/> 
 
         <button> Calcular </button>
-        <button type="reset"> limpar </button>
+        <button type="reset">limpar</button>
     </form>
+    </div>
 </body>
 </html>
+
